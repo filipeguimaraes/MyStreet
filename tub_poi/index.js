df2f4df5b86195e06fc4a6e644d962980a0ae2de
Filipe Guimarães <fmtfg99@gmail.com>
2021-05-15 15:59:47 +0100

    relations fix

--- tub_poi/index.js
+++ tub_poi/index.js
@@ -7,7 +7,7 @@ const city = 'Porto'
     Method that retrieves information from the Overpass API and stores it in the cache directory.
     Checks if the directory of a city already exists, if not, it fetches de data and creates the new directory.
  */
-function getsData(city){
+function getsData(city) {
 
     let dir = "cache/" + city;
 
@@ -19,17 +19,18 @@ function getsData(city){
         //Get route and write to ./city/route.json file
         getRoutes(city).then((data) => {
             let dadosR = JSON.stringify(data);
-            fs.writeFileSync(dir+'/routes.json', dadosR);
+            fs.writeFileSync(dir + '/routes.json', dadosR);
         }).catch(console.log)
 
         //Get stations and write to ./city/stations.json file
         getStations(city).then((data) => {
             let dadosS = JSON.stringify(data);
-            fs.writeFileSync(dir+'/stations.json', dadosS);
+            fs.writeFileSync(dir + '/stations.json', dadosS);
         }).catch(console.log)
     }
 
 }
+
 /*
     Argument: city
     Method that reads through the stations file and retrieves all the essential information
@@ -37,42 +38,35 @@ function getsData(city){
     ATTENTION ERROR: Ao escrever o ficheiro vai ser acrescentada um virgula a mais no final.
                      Arranjar isso depois.
  */
-function writeStations() {
-    let objData = '';
-//cache/Land/Land.json
-    fs.readFile('cache/Porto/stations.json', 'utf8', (err, jsonString) => {
-        if (err) {
-            console.log("Error reading file from disk:", err)
-            return
-        }
-        try {
-            // parse string JSON into a javascript object
-            objData = JSON.parse(jsonString)
-
-            fs.appendFileSync('cache/Porto/stationsFinal.json', '[');
-
-            for (const elem in objData) {
-
-                const dados = {
-                     id: objData[elem].id,
-                     lat: objData[elem].lat,
-                     lon: objData[elem].lon,
-                     name: objData[elem].tags.name}
-                console.log(dados)
-
-                // parsing a javascript to a JSON string
-                let dadosS = JSON.stringify(dados);
-                fs.appendFileSync('cache/Land/stationsFinal.json', dadosS);
-
-                //ERROR
-                fs.appendFileSync('cache/Land/stationsFinal.json', ',');
+async function finalStations() {
+    return new Promise((resolve, reject) => {
+        fs.readFile('cache/Porto/stations.json', 'utf8', (err, jsonString) => {
+            if (err) {
+                console.log("Error open File");
             }
-            fs.appendFileSync('cache/Land/stationsFinal.json', ']');
+            try {
+                // parse string JSON into a javascript object
+                let objData = JSON.parse(jsonString)
 
-        } catch (err) {
-            console.log('Error parsing JSON string:', err)
-        }
-    })
+                let result = [];
+                for (const elem in objData) {
+
+                    let dados = {
+                        id: objData[elem].id,
+                        lat: objData[elem].lat,
+                        lon: objData[elem].lon,
+                        name: objData[elem].tags.name,
+                        relations: []
+                    }
+
+                    result.push(dados);
+                }
+                resolve(result);
+            } catch (err) {
+                console.log('Error parsing JSON string:', err)
+            }
+        });
+    });
 }
 
 /*
@@ -80,142 +74,44 @@ function writeStations() {
     the corresponding routes.
  */
 
-function getRelations(){
-
-    let objDataStation;
-    let objDataRoute;
-    let dic = new Map()
-    let idRoutes=[]
-    let counter=0;
-    let counterS=0;
-    let idR;
-    let idStation
-
-    // Get the stations
-    fs.readFile('cache/Porto/stationsFinal.json', 'utf8', (err, jsonStringStation) => {
-        if (err) {
-            console.log("Error reading file from disk:", err)
-            return
-        }
-        try {
-            objDataStation = JSON.parse(jsonStringStation)
-            // Get the routes
-            fs.readFile('cache/Porto/routes.json', 'utf8', (err, jsonStringRoute) => {
+async function getRelations(stations) {
+    return new Promise((resolve, reject) => {
+        fs.readFile('cache/Porto/routes.json', 'utf8',
+            (err, jsonStringRoute) => {
                 if (err) {
                     console.log("Error reading file from disk:", err)
-                    return
+                    return;
                 }
                 try {
-                    for(const station in objDataStation) {
-                        idStation = objDataStation[station].id
-                        counterS = counterS + 1;
-                        objDataRoute = JSON.parse(jsonStringRoute)
-                        for (let object in objDataRoute) {
-                            if (objDataRoute[object].type == 'relation') {
-                                idR = objDataRoute[object].id
-                                counter = counter + 1;
-                                let members = objDataRoute[object].members
+                    let objDataRoute = JSON.parse(jsonStringRoute);
+                    for (const i in stations) {
+                        let idStation = stations[i].id;
+                        for (let j in objDataRoute) {
+                            if (objDataRoute[j].type === 'relation') {
+                                let idRelation = objDataRoute[j].id;
+                                let members = objDataRoute[j].members;
                                 for (let m in members) {
-                                    if (members[m].type == 'node' && members[m].ref == idStation){
-                                        idRoutes.push(idR)
+                                    if (members[m].type === "node"
+                                        && members[m].ref === idStation) {
+                                        stations[i].relations.push(idRelation);
                                     }
                                 }
                             }
                         }
-
-                        let uniqueIds = [...new Set(idRoutes)];
-
-                        // Dictionary to store the key:station, value:list of id routes pair
-                        //dic.set(idStation,uniqueIds)
-                        //console.log(uniqueIds)
-                        console.log('Escrever entry ...')
-                        const entry = {
-                            station: idStation,
-                            relations: uniqueIds}
-                        let data = JSON.stringify(entry);
-                        fs.appendFileSync('cache/Porto/Dicionario.json', data);
-                        console.log('Finished')
                     }
-                    //console.log('Num stations:' + counterS)
-                    //console.log('Num relations:' + counter)
+                    resolve(stations);
                 } catch (err) {
                     console.log('Error parsing JSON string:', err)
                 }
-            })
-        } catch (err) {
-            console.log('Error parsing JSON string:', err)
-        }
-    })
-/*
-    try{
-        for (let [key, value] of dic) {
-            const entry = {
-                station: key,
-                relations: value}
-            let data = JSON.stringify(entry);
-            fs.appendFileSync('cache/Porto/Dicionario.json', data);
-
-            console.log('Escrevendo ...')
-        }
-
-    } catch (err) {
-        console.log('Error parsing JSON string:', err)
-    }*/
-
+            });
+    });
 }
 
-//getsData('Porto')
-//writeStations()
-getRelations()
+finalStations().then(stations => {
+    getRelations(stations).then(console.log);
+});
+
+//getRelations().then(console.log);
 
 
 
-
-
-//
-/*
-    //para cada estação, retirar os dados principais e escrever no ficheiro
-
-    for(var elem in objData.elements){
-         //console.log(elem)
-         const dados = {
-             id: elem.id
-             lat: elem.lat
-             lon: elem.lon
-             name: elem.tags.name
-          -> relations: [ {idX, nome da relação (trajeto)} ... ]
-         }
-         let station = JSON.stringify(dados);
-         fs.writeFileSync('cache/' + city +'/finalStations.json', station);
-
-    // rotas
-
-   //iterar sobre os elementos -> relations
-           idIterationAtual = para saber onde guardar ?
-        //iterar sobre os membros da relation
-
-            para cada valor ref,
-
-         */
-
-//console.log(JSON.parse(fs.readFileSync('cache/Porto/stations.json', 'utf-8')))
-
-/*
-try {
-                    objDataRoute = JSON.parse(jsonStringRoute)
-                    // Go through every station and keep its id
-                    for(const station in objDataStation){
-                        const idStation = objDataStation[station].id
-                        // Go through every relation
-                        for(const eachRelation in objDataRoute){
-                            const idR = objDataRoute[eachRelation].id
-                            let members = objDataRoute[eachRelation].members
-                            // For every member in the relation
-                            for(const m in members){
-                                if(members[m].type=='node' && members[m].ref == idStation){
-                                    console.log('Found match: node with num ' + members[m].ref )
-                                    idRoutes.push(idR)
-                                }
-                            }
-                        }
- */
